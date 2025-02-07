66d357807fcb67d7e9c5baae4bf3dbf30c34987c
Ta25Li <2885921@gmail.com>
2025-02-07 18:54:49 +0200

    Added login Sucsess - 5 feb

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,10 +1,46 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-public class HelperUser extends HelperBase{
+public class HelperUser extends HelperBase {
 
     public HelperUser(WebDriver wd) {
         super(wd);
+    }
+
+    public void openLogRegForm() {
+//        WebElement loginTab = wd.findElement(By.cssSelector("a[href='/login']"));
+//        //xPath -->> //a[text()='LOGIN']
+//        loginTab.click();
+    click(By.cssSelector("a[href='/login']"));
+    }
+
+    public void fillLogRegForm(String email, String password) {
+
+        //WebElement emailInput = wd.findElement(By.name("email"));
+//emailInput.click();
+//emailInput.clear();
+//emailInput.sendKeys(email);
+        type(By.name("email"), email);
+
+//WebElement passwordInput = wd.findElement(By.xpath("//input[last()]")); // css  -> name("password")
+//passwordInput.click();
+//passwordInput.clear();
+//passwordInput.sendKeys(password);
+        type(By.xpath("//input[last()]"), password);
+    }
+
+    public void submitLogin(){ //find btn Login and click
+click(By.xpath("//button[text()='Login']"));
+    }
+
+    public boolean isLogged() {
+     return isElementPresent(By.xpath("//button[text()='Sign Out']"));
+    }
+
+    public void logout() {
+        click(By.xpath("//button[text()='Sign Out']"));
     }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,11 +1,49 @@
 package tests;
+//leya@bach.com
+//leyaBach9!
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class LoginTests extends TestBase{
+public class LoginTests extends TestBase {
+
+    @BeforeMethod
+    public void preCondition(){
+        //if SignOut present --> logout
+        if (app.getHelperUser().isLogged()) // if is logged true - then do log out
+            app.getHelperUser().logout();
+
+
+    }
+    @Test
+    public void loginSuccess() {
+        app.getHelperUser().openLogRegForm();
+        app.getHelperUser().fillLogRegForm("leya@bach.com", "leyaBach9!");
+        app.getHelperUser().submitLogin();
+
+        //Assert
+//        Assert.assertEquals();
+//        Assert.assertNotEquals();
+//        Assert.assertTrue();
+//        Assert.assertFalse();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
     @Test
-    public void test(){
+    public void loginSuccessModel() {
+        app.getHelperUser().openLogRegForm();
+        app.getHelperUser().fillLogRegForm("leya@bach.com", "leyaBach9!");
+        app.getHelperUser().submitLogin();
 
+     Assert.assertTrue(app.getHelperUser().isLogged());
     }
 }
